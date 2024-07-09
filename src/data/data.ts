662c4c91097cea1b0fb324e0f885a1ff7948a452
seongjin <chosj@thingspire.com>
2024-07-09 21:42:59 +0900

    feat: 각 항목별 Text 데이터 추가

---
+++ src/data/data.ts
@@ -0,0 +1,112 @@
+export const introData = {
+    keyHistory: [
+        'React,Next.js를 2년간 현업 개발',
+        'HighChart,MUI 등의 라이브러리를 활용한 대시보드 데이터 시각화 UI 개발',
+        '정보 처리 기사 취득[2022.06]',
+    ],
+};
+
+export const aboutMeData = {
+    interview: [
+        {
+            question: 'Q. 주로 무슨 업무를 해왔나요?',
+            answer: '주로 대시보드 형 인터페이스를 개발해왔습니다. React와 Next.js를 활용해 SPA방식으로 서비스를 제공해왔고, HighChart같은 차트 라이브러리를 통한 데이터 시각화 작업을 주로 해왔습니다.',
+        },
+        {
+            question: 'Q. 프론트엔드 직무를 선택하게된 이유가 무엇인가요?',
+            answer: '백엔드 개발자 분들에게 여쭤 봤을 때 대부분 "프론트엔드를 기피하는 이유가 계속해서 트랜드를 따라가야하는 부분이 백엔드에 비해 비중이 큰 것 같고, css 같이 디자인 적인 요소를 챙기는 것이 적성에 맞지 않는다"라는 부분이 많은데 저는 이런 요소들이 오히려 저에게 흥미를 돋우는 부분들이라 프론트엔드 직무를 선택하게 되었습니다.',
+        },
+        {
+            question: 'Q. 일을 함에 있어 무엇을 가장 중요시 하나요?',
+            answer: '"소통"입니다. 일을 계속 해오면서 느낀 것이 무엇보다 소통이 되야한다는 것이었습니다. 특히나 프론트엔드는 기획자,디자이너,백엔드 개발자 등 다양한 직무자들 사이에서 프록시 서버같은 역할읗하게 될 때가 많은데 이게 원활하지 않으면 서로의 작업 진행 사항, 진행 방향 등을 알 수 없어 재개발을 하거나 수정사항이 생기는 불상사가 많았기에 이를 줄이는 것이 중요하다고 생각합니다.',
+        },
+    ],
+};
+
+export const skillData = {
+    list: [
+        {
+            iconImage: ['html', 'css'],
+            title: 'HTML & CSS',
+            description: [
+                'Redux-toolkit의 RTK-query 사용에 익숙합니다.',
+                'Next.js를 활용한 SSR 활용 경험이 있습니다.',
+                'Google Analytics를 활용한 MAU 체크 및 사용자의 주요 사용 서비스를 ',
+                'React 프로젝트의 Frontend Part를 처음부터 끝까지 참여하고 운영',
+            ],
+        },
+        {
+            iconImage: ['react'],
+            title: 'React',
+            description: [
+                'Redux-toolkit의 RTK-query 사용에 익숙합니다.',
+                'Next.js를 활용한 SSR 활용 경험이 있습니다.',
+                'Google Analytics를 활용한 MAU 체크 및 사용자의 주요 사용 서비스를 ',
+                'React 프로젝트의 Frontend Part를 처음부터 끝까지 참여하고 운영',
+            ],
+        },
+        {
+            iconImage: ['javascript'],
+            title: 'Javascript',
+            description: [
+                'Redux-toolkit의 RTK-query 사용에 익숙합니다.',
+                'Next.js를 활용한 SSR 활용 경험이 있습니다.',
+                'Google Analytics를 활용한 MAU 체크 및 사용자의 주요 사용 서비스를 ',
+                'React 프로젝트의 Frontend Part를 처음부터 끝까지 참여하고 운영',
+            ],
+        },
+        {
+            iconImage: ['aws'],
+            title: 'Infra',
+            description: [
+                'Redux-toolkit의 RTK-query 사용에 익숙합니다.',
+                'Next.js를 활용한 SSR 활용 경험이 있습니다.',
+                'Google Analytics를 활용한 MAU 체크 및 사용자의 주요 사용 서비스를 ',
+                'React 프로젝트의 Frontend Part를 처음부터 끝까지 참여하고 운영',
+            ],
+        },
+    ],
+};
+
+export const projectLeftContents = [
+    {
+        title: 'CEMS',
+        stack: [
+            {
+                url: '1',
+                imageName: 'nextjs',
+                name: 'Next.js',
+            },
+            {
+                url: '2',
+                imageName: 'redux',
+                name: 'Redux',
+            },
+        ],
+        description:
+            '웹 디자이너로 활동하며 사용자 중심 디자인,**협업,프론트엔드 기초지식을 쌓았습니다.사용자와의 소통**에 대해 중요성을 인지하고, 더 밀접하게 상호작용 가능한 프로젝트**를 개발하고자 프론트엔드 개발자로 전향을 결심하게 되었습니다.',
+    },
+    {
+        title: 'Sseong',
+        stack: [
+            {
+                url: '1',
+                imageName: 'nextjs',
+                name: 'Next.js',
+            },
+            {
+                url: '2',
+                imageName: 'redux',
+                name: 'Redux',
+            },
+        ],
+        description:
+            '웹 디자이너로 활동하며 사용자 중심 디자인,**협업,프론트엔드 기초지식을 쌓았습니다.사용자와의 소통**에 대해 중요성을 인지하고, 더 밀접하게 상호작용 가능한 프로젝트**를 개발하고자 프론트엔드 개발자로 전향을 결심하게 되었습니다.**React, Styled-Component, Redux Toolkit 등**다양한 프레임워크와 라이브러리를 도입해 발전할 수 있는**새로운 도전에 나서고 있습니다.',
+    },
+];
+
+export const projectRightContents = [
+    'https://raw.githubusercontent.com/chosj-dev/seongjin-portfolio/main/src/assets/images/cems-image-1.jpg',
+    'https://raw.githubusercontent.com/chosj-dev/seongjin-portfolio/main/src/assets/images/cems-image-2.jpg',
+    'https://raw.githubusercontent.com/chosj-dev/seongjin-portfolio/main/src/assets/images/cems-image-3.jpg',
+];
