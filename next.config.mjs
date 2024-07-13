80c7c2a46a7467cb8be4a604e0fab2f222d912cc
chosj-dev <chosjdev01@gmail.com>
2024-07-13 18:07:48 +0900

    chore: config 내용 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  swcMinify: true,
-  compiler: {
-    styledComponents: true,
-  },
+    swcMinify: true,
+    compiler: {
+        styledComponents: true,
+    },
+    images: {
+        domains: ['raw.githubusercontent.com'],
+    },
 };
 
 export default nextConfig;
